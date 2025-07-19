2b2f94fc701c144ce407a0f6c974d7a54aef61d7
mahir-dhiyan <mahird.chowdhury@gmail.com>
2025-07-19 22:06:27 +0600

    dynamic url implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from tasks.views import show_task
+from tasks.views import show_specific_task
+# from tasks.views import heda
 urlpatterns=[
-    path('show-task/',show_task)
+    path('show-task/',show_task),
+    path('show-task/<int:id>',show_specific_task)
+    # path('hedar-url',heda)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,9 @@ def contact(request):
     return HttpResponse("<h1 style = 'color: red'>This is contact Page</h1>")
 def show_task(request):
     return HttpResponse("This is our task page")
+def show_specific_task(request,id):
+    print("id",id)
+    print("Id type: ", type(id))
+    return HttpResponse(f"This is specific task page {id}")
+# def heda(request):
+#     return HttpResponse("This is hedar baaal")
